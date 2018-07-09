446e5102ed8bd727ea0d09eda446c9de436e6120
Zachariah Jeyakaran <zachariah.jeyakaran@citrix.com>
2018-07-09 19:56:57 -0400

    custom mappings

--- Sqlite.Fast/ColumnToFieldMap.cs
+++ Sqlite.Fast/ColumnToFieldMap.cs
@@ -1,25 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 
 namespace Sqlite.Fast
 {
     internal interface IColumnToFieldMap<TRecord>
     {
-        void AssignInteger(ref TRecord rec, long value);
-        void AssignFloat(ref TRecord rec, double value);
-        void AssignText(ref TRecord rec, Span<byte> value);
-        void AssignBlob(ref TRecord rec, Span<byte> value);
+        void AssignInteger(ref TRecord rec, long data);
+        void AssignFloat(ref TRecord rec, double data);
+        void AssignText(ref TRecord rec, Span<byte> data);
+        void AssignBlob(ref TRecord rec, Span<byte> data);
         void AssignNull(ref TRecord rec);
     }
 
-    public delegate T IntegerConverter<T>(long value);
-    public delegate T FloatConverter<T>(double value);
-    public delegate T TextConverter<T>(Span<byte> value);
-    public delegate T BlobConverter<T>(Span<byte> value);
+    public delegate T IntegerConverter<T>(long data);
+    public delegate T FloatConverter<T>(double data);
+    public delegate T TextConverter<T>(Span<byte> data);
+    public delegate T BlobConverter<T>(Span<byte> data);
     public delegate T NullConverter<T>();
 
-    public delegate void FieldAssigner<Record, Field>(ref Record rec, Field value);
+    public delegate void FieldAssigner<TRecord, TField>(ref TRecord rec, TField data);
 
     internal class ColumnToFieldMap<TRecord, TField> : IColumnToFieldMap<TRecord>
     {
@@ -31,10 +33,142 @@ namespace Sqlite.Fast
 
         private readonly FieldAssigner<TRecord, TField> _assign;
 
-        public void AssignInteger(ref TRecord rec, long value) => _assign(ref rec, _convertInteger(value));
-        public void AssignFloat(ref TRecord rec, double value) => _assign(ref rec, _convertFloat(value));
-        public void AssignText(ref TRecord rec, Span<byte> value) => _assign(ref rec, _convertText(value));
-        public void AssignBlob(ref TRecord rec, Span<byte> value) => _assign(ref rec, _convertBlob(value));        
+        public ColumnToFieldMap(
+            IntegerConverter<TField> convertInteger,
+            FloatConverter<TField> convertFloat,
+            TextConverter<TField> convertText,
+            BlobConverter<TField> convertBlob,
+            NullConverter<TField> convertNull,
+            FieldAssigner<TRecord, TField> assign)
+        {
+            _convertInteger = convertInteger;
+            _convertFloat = convertFloat;
+            _convertText = convertText;
+            _convertBlob = convertBlob;
+            _convertNull = convertNull;
+
+            _assign = assign;
+        }
+
+        public void AssignInteger(ref TRecord rec, long data) => _assign(ref rec, _convertInteger(data));
+        public void AssignFloat(ref TRecord rec, double data) => _assign(ref rec, _convertFloat(data));
+        public void AssignText(ref TRecord rec, Span<byte> data) => _assign(ref rec, _convertText(data));
+        public void AssignBlob(ref TRecord rec, Span<byte> data) => _assign(ref rec, _convertBlob(data));        
         public void AssignNull(ref TRecord rec) => _assign(ref rec, _convertNull());
+    }
+
+    internal static class ColumnToFieldMap
+    {
+        public static IBuilder<TRecord> Create<TRecord>(MemberInfo member)
+        {
+            Type memberType;
+            if(member is PropertyInfo property)
+            {
+                memberType = property.PropertyType;
+            }
+            else if(member is FieldInfo field)
+            {
+                memberType = field.FieldType;
+            }
+            else
+            {
+                throw new NotSupportedException(member.MemberType.ToString());
+            }
+            var constructor = typeof(Builder<,>)
+                .MakeGenericType(new[] { typeof(TRecord), memberType })
+                .GetTypeInfo()
+                .GetConstructor(new[] { typeof(MemberInfo) });
+            return (IBuilder<TRecord>)constructor.Invoke(new[] { member });
+        }
+
+        internal interface IBuilder<TRecord>
+        {
+            MemberInfo Member { get; }
+            IColumnToFieldMap<TRecord> Compile();
+            Builder<TRecord, TField> AsConcrete<TField>();
+        }
+
+        internal class Builder<TRecord, TField> : IBuilder<TRecord>
+        {
+            public MemberInfo Member { get; }
+
+            private IntegerConverter<TField> _integerConverter;
+            private FloatConverter<TField> _floatConverter;
+            private TextConverter<TField> _textConverter;
+            private BlobConverter<TField> _blobConverter;
+            private NullConverter<TField> _nullConverter;
+
+            public Builder(MemberInfo member)
+            {
+                Member = member;
+            }
+
+            public IColumnToFieldMap<TRecord> Compile()
+            {
+                return new ColumnToFieldMap<TRecord, TField>(
+                    _integerConverter,
+                    _floatConverter,
+                    _textConverter,
+                    _blobConverter,
+                    _nullConverter,
+                    CompileAssigner(Member));
+            }
+
+            private static FieldAssigner<TRecord, TField> CompileAssigner(MemberInfo memberInfo)
+            {
+                var record = Expression.Parameter(typeof(TRecord).MakeByRefType());
+                Expression member;
+                if (memberInfo is FieldInfo fieldInfo)
+                {
+                    member = Expression.Field(record, fieldInfo);
+                }
+                else if (memberInfo is PropertyInfo propertyInfo)
+                {
+                    member = Expression.Property(record, propertyInfo);
+                }
+                else
+                {
+                    throw new Exception($"Cannot assign to {memberInfo.MemberType}");
+                }
+                var value = Expression.Parameter(typeof(TField));
+                var assignment = Expression.Assign(member, value);
+                var lambda = Expression.Lambda<FieldAssigner<TRecord, TField>>(assignment, record, value);
+                return lambda.Compile();
+            }
+
+            public void SetIntegerConverter(IntegerConverter<TField> integerConverter)
+            {
+                _integerConverter = integerConverter;
+            }
+
+            public void SetFloatConverter(FloatConverter<TField> floatConverter)
+            {
+                _floatConverter = floatConverter;
+            }
+
+            public void SetTextConverter(TextConverter<TField> textConverter)
+            {
+                _textConverter = textConverter;
+            }
+
+            public void SetBlobConverter(BlobConverter<TField> blobConverter)
+            {
+                _blobConverter = blobConverter;
+            }
+
+            public void SetNullConverter(NullConverter<TField> nullConverter)
+            {
+                _nullConverter = null;
+            }
+
+            public Builder<TRecord, TCallerField> AsConcrete<TCallerField>()
+            {
+                if (this is Builder<TRecord, TCallerField> builder)
+                {
+                    return builder;
+                }
+                throw new ArgumentException($"Field is {typeof(TField).Name} not {typeof(TCallerField).Name}");
+            }
+        }
     }
 }
--- Sqlite.Fast/RowToRecordMap.cs
+++ Sqlite.Fast/RowToRecordMap.cs
@@ -1,11 +1,129 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 
 namespace Sqlite.Fast
 {
     public class RowToRecordMap<TRecord>
     {
-        internal IColumnToFieldMap<TRecord>[] ColumnMaps { get; } 
+        internal IColumnToFieldMap<TRecord>[] ColumnMaps { get; }
+
+        internal RowToRecordMap(IEnumerable<IColumnToFieldMap<TRecord>> columnMaps)
+        {
+            ColumnMaps = columnMaps.ToArray();
+        }
+    }
+
+    public static class RowToRecordMap
+    {
+        public static Builder<TRecord> Empty<TRecord>() => new Builder<TRecord>();
+
+        public static Builder<TRecord> Default<TRecord>()
+        {
+            var builder = new Builder<TRecord>();
+            builder.SetDefaultMappings();
+            return builder;
+        }
+
+        public class Builder<TRecord>
+        {
+            private readonly List<ColumnToFieldMap.IBuilder<TRecord>> _columnMapBuilders = new List<ColumnToFieldMap.IBuilder<TRecord>>();
+
+            internal Builder()
+            {
+                if (typeof(TRecord).GetTypeInfo().StructLayoutAttribute.Value
+                    == System.Runtime.InteropServices.LayoutKind.Auto)
+                {
+                    throw new ArgumentException($"Target type {typeof(TRecord).Name} must have sequential or explicit StructLayout");
+                }
+            }
+
+            internal void SetDefaultMappings()
+            {
+                throw new NotImplementedException();
+            }
+
+            private ColumnToFieldMap.IBuilder<TRecord> GetOrAdd(MemberInfo member)
+            {
+                var columnMap = _columnMapBuilders.Find(cm => cm.Member == member);
+                if (columnMap == null)
+                {
+                    columnMap = ColumnToFieldMap.Create<TRecord>(member);
+                    _columnMapBuilders.Add(columnMap);
+                }
+                return columnMap;
+            }
+
+            private ColumnToFieldMap.Builder<TRecord, TField> GetOrAdd<TField>(Expression<Func<TRecord, TField>> propertyOrField)
+            {
+                return GetOrAdd(GetMemberInfo(propertyOrField)).AsConcrete<TField>();
+            }
+
+            public RowToRecordMap<TRecord> Compile()
+            {
+                var columnMaps = new List<IColumnToFieldMap<TRecord>>(capacity: _columnMapBuilders.Count);
+                foreach (var colBuilder in _columnMapBuilders)
+                {
+                    IColumnToFieldMap<TRecord> colMap = colBuilder.Compile();
+                    columnMaps.Add(colMap);
+                }
+                return new RowToRecordMap<TRecord>(columnMaps);
+            }
+
+            public Builder<TRecord> Custom<TField>(Expression<Func<TRecord, TField>> propertyOrField, IntegerConverter<TField> integerConverter)
+            {
+                GetOrAdd(propertyOrField).SetIntegerConverter(integerConverter);
+                return this;
+            }
+
+            public Builder<TRecord> Custom<TField>(Expression<Func<TRecord, TField>> propertyOrField, FloatConverter<TField> floatConverter)
+            {
+                GetOrAdd(propertyOrField).SetFloatConverter(floatConverter);
+                return this;
+            }
+
+            public Builder<TRecord> Custom<TField>(Expression<Func<TRecord, TField>> propertyOrField, TextConverter<TField> textConverter)
+            {
+                GetOrAdd(propertyOrField).SetTextConverter(textConverter);
+                return this;
+            }
+
+            public Builder<TRecord> Custom<TField>(Expression<Func<TRecord, TField>> propertyOrField, BlobConverter<TField> blobConverter)
+            {
+                GetOrAdd(propertyOrField).SetBlobConverter(blobConverter);
+                return this;
+            }
+
+            public Builder<TRecord> Custom<TField>(Expression<Func<TRecord, TField>> propertyOrField, NullConverter<TField> nullConverter)
+            {
+                GetOrAdd(propertyOrField).SetNullConverter(nullConverter);
+                return this;
+            }
+
+            private static MemberInfo GetMemberInfo<TField>(Expression<Func<TRecord, TField>> propertyOrField)
+            {
+                if (propertyOrField.Body is MemberExpression member && ShouldMapMember(member.Member))
+                {
+                    return member.Member;
+                }
+                throw new ArgumentException($"Expression is not get/set-able field or property of {typeof(TRecord).Name}");
+            }
+
+            private static bool ShouldMapMember(MemberInfo member)
+            {
+                if (member is PropertyInfo property)
+                {
+                    return property.CanRead && property.CanWrite;
+                }
+                else if (member is FieldInfo field)
+                {
+                    return !field.IsInitOnly;
+                }
+                return false;
+            }
+        }
     }
 }
