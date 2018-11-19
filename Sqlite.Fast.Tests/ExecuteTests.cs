5739364c3c8aecb8e294aa3540aaae3c2a629dba
Zachariah Jeyakaran <zachariah.jeyakaran@citrix.com>
2018-11-19 16:38:22 -0500

    reuse tests

--- Sqlite.Fast.Tests/ExecuteTests.cs
+++ Sqlite.Fast.Tests/ExecuteTests.cs
@@ -87,5 +87,83 @@ namespace Sqlite.Fast.Tests
                 Assert.Equal(6, sum);
             }
         }
+
+        [Fact]
+        public void Enumerate_Rebind()
+        {
+            using (var tbl = new TestTable("create table t (x int)"))
+            using (var insert = tbl.Stmt("insert into t values(@x)"))
+            using (var select = tbl.Stmt("select x from t where x=@x"))
+            {
+                insert.Bind(0, 1).Execute();
+                insert.Bind(0, 2).Execute();
+                R<int> r = default;
+                var enumerator = select.Bind(0, 1).Execute(r.Map).GetEnumerator();
+                Assert.True(enumerator.MoveNext());
+                enumerator.Current.AssignTo(ref r);
+                Assert.Equal(1, r.Value);
+
+                select.Bind(0, 2).Execute(r.Map, ref r);
+                Assert.Equal(2, r.Value);
+            }
+        }
+        
+        [Fact]
+        public void Rebind_Enumerate_Throws()
+        {
+            using (var tbl = new TestTable("create table t (x int)"))
+            using (var insert = tbl.Stmt("insert into t values(@x)"))
+            using (var select = tbl.Stmt("select x from t where x=@x"))
+            {
+                insert.Bind(0, 1).Execute();
+                insert.Bind(0, 2).Execute();
+                R<int> r = default;
+                var enumerator = select.Bind(0, 1).Execute(r.Map).GetEnumerator();
+                Assert.True(enumerator.MoveNext());
+                enumerator.Current.AssignTo(ref r);
+                Assert.Equal(1, r.Value);
+                select.Bind(0, 2);
+                Assert.Throws<Exception>(() => enumerator.MoveNext());
+            }
+        }
+
+        [Fact]
+        public void Enumerate_Reexecute()
+        {
+            using (var tbl = new TestTable("create table t (x int)"))
+            using (var insert = tbl.Stmt("insert into t values(@x)"))
+            using (var select = tbl.Stmt("select sum(x) from t"))
+            {
+                insert.Bind(0, 1).Execute();
+                insert.Bind(0, 2).Execute();
+                R<int> r = default;
+                var enumerator = select.Execute(r.Map).GetEnumerator();
+                Assert.True(enumerator.MoveNext());
+                enumerator.Current.AssignTo(ref r);
+                Assert.Equal(3, r.Value);
+                r = default;
+                select.Execute(r.Map, ref r);
+                Assert.Equal(3, r.Value);
+            }
+        }
+
+        [Fact]
+        public void Reexecute_Enumerate_Throws()
+        {
+            using (var tbl = new TestTable("create table t (x int)"))
+            using (var insert = tbl.Stmt("insert into t values(@x)"))
+            using (var select = tbl.Stmt("select sum(x) from t"))
+            {
+                insert.Bind(0, 1).Execute();
+                insert.Bind(0, 2).Execute();
+                R<int> r = default;
+                var enumerator = select.Execute(r.Map).GetEnumerator();
+                Assert.True(enumerator.MoveNext());
+                enumerator.Current.AssignTo(ref r);
+                Assert.Equal(3, r.Value);
+                select.Execute(r.Map, ref r);
+                Assert.Throws<Exception>(() => enumerator.MoveNext());
+            }
+        }
     }
 }
